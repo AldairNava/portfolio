95b3aee1455d5012c0be035d06bda4e0a091b0f6
Aldaie <brandon.nava@desarrollatech.com>
2023-10-25 10:58:29 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+
+const menuOpciones = document.querySelector(".menu-opciones");
+const btncontacto = document.getElementById("contacto");
+const header = document.querySelector("header");
+const controlesUsuario = document.querySelector(".controles-usuario");
+const contenedor = document.querySelector(".contenedor");
+const btnMenu = document.getElementById("btn-menu");
+const btncv = document.getElementById("btncv")
+
+const responsiveY = ()=>{
+    if(window.innerHeight<=362){
+        if(menuOpciones.classList.contains("mostrar"))
+            menuOpciones.classList.add("min");
+        else
+            menuOpciones.classList.remove("min");
+    }
+    else{
+        menuOpciones.classList.remove("min");
+    }
+};
+const responsive = ()=>{
+    if(window.innerWidth<=1150){
+        menuOpciones.children[0].appendChild(btncontacto);
+        menuOpciones.children[0].appendChild(btncv);
+        header.appendChild(menuOpciones);
+    }else{
+        controlesUsuario.appendChild(btncontacto);
+        controlesUsuario.appendChild(btncv);
+        contenedor.appendChild(menuOpciones);
+    }
+
+    responsiveY();
+}
+
+btnMenu.addEventListener("click",()=>{
+    menuOpciones.classList.toggle("mostrar");
+    responsiveY();
+});
+responsive();
+
+window.addEventListener("resize",responsive);
