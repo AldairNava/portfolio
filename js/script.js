5e84228c99976726f21c31ade8c1013cf362fa13
aldair nava <aldairnava.18@outlook.com>
2024-01-05 17:14:52 -0600

    completar

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const controlesUsuario = document.querySelector(".controles-usuario");
 const contenedor = document.querySelector(".contenedor");
 const btnMenu = document.getElementById("btn-menu");
 const btncv = document.getElementById("btncv")
+const btnport = document.getElementById("portfolio")
 
 const responsiveY = ()=>{
     if(window.innerHeight<=362){
@@ -22,10 +23,12 @@ const responsive = ()=>{
     if(window.innerWidth<=1150){
         menuOpciones.children[0].appendChild(btncontacto);
         menuOpciones.children[0].appendChild(btncv);
+        menuOpciones.children[0].appendChild(btnport);
         header.appendChild(menuOpciones);
     }else{
         controlesUsuario.appendChild(btncontacto);
         controlesUsuario.appendChild(btncv);
+        controlesUsuario.appendChild(btnport);
         contenedor.appendChild(menuOpciones);
     }
 
